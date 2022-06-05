62ade1cddd6c744c204ebdcc96ce97eaaaa7d2a4
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2022-06-05 12:36:47 -0500

    Completed assignment

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -5,13 +5,23 @@ function MultiButton() {
     console.log(`Button ${number} was clicked`);
   }
 
+  // return (
+  //   <div>
+  //     <button onClick={handleClick}>Button 1</button>
+  //     <button onClick={handleClick}>Button 2</button>
+  //     <button onClick={handleClick}>Button 3</button>
+  //   </div>
+  // );
+  
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
+
+
 }
 
 export default MultiButton;
--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+// ---> This is one way 
+
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
+//   return <button>Tickle me!</button>;
+// }
+
+// ---> This will also work
+
+// function Tickler() {
+//   return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
+// }
+
+// ---> And this 
+
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  function tickle(event) {
+    console.log(event);
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
